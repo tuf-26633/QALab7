867e2aa10ad0a82c38135c2ecb4522e3c365b4ed
Rebecca Fernandez <rebecca.fernandez@temple.edu>
2018-10-23 19:17:05 -0400

    Initial commit

---
+++ QALab7/TicTacToe.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QALab7
+{
+    class TicTacToe
+    {
+
+        int[] board = new int[9];
+        public int turn;
+
+
+        // fills the 3x3 board with 0's indicating empty spaces
+
+        public void CreateBoard()
+        {
+            for (int x = 0; x < board.Length; x++)
+            {
+                board[x] = 0;
+            }
+        }
+
+
+        /*
+         * player X goes first
+         */
+
+        public char CurrentPlayer()
+        {
+            if (turn % 2 == 1)
+                return 'X';
+
+            else return 'O';
+
+        }
+
+        public void ChangePlayer()
+        {
+            turn++;
+        }
+
+
+        /*
+         * player types in name (X or O) and selects space
+         * X marked as 1
+         * O marked as 2
+         */
+
+        public void MakeMove(char p, int space)
+        {
+            if (p == 'X')
+            {
+                board[space] = 1;
+            }
+
+            if (p == 'O')
+            {
+                board[space] = 2;
+            }
+        }
+
+        /* 
+         * 
+         * returns true if there is an X or an O in the board space
+         */
+
+        public bool SpaceInUse(int x)
+        {
+            return board[x] != 0;
+        }
+
+    }
+}
---
+++ QALab7UnitTests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QALab7UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NineActiveFields()
+        {
+
+        }
+
+        [TestMethod]
+        public void CurrentPlayerX()
+        {
+
+        }
+
+        [TestMethod]
+        public void CurrentPlayerO()
+        {
+
+        }
+
+        [TestMethod]
+        public void PlacesX()
+        {
+
+        }
+
+        [TestMethod]
+        public void PlacesO()
+        {
+
+        }
+
+        [TestMethod]
+        public void UnoccupiedSpot()
+        {
+
+        }
+    }
+}
